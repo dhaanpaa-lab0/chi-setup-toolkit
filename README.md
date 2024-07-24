700bdec355612fe39a8e1c5c387f28a7c38cae8f
Daniel Haanpaa <dhaanpaa@djhent.us>
2024-07-23 19:18:18 -0700

    Setup Project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # chi-setup-toolkit
+
 CHI (Container Hosting Infrastructure) Toolkit for setting up container hosts
+
+## Pre-Requistes
+
+- Ubuntu 22.04 or 24.04 host
+- User account must have passwordless sudo or able to switch to root account to run bootstrap script
+
+## Steps to use
+
+1. Run `curl `
---
+++ bootstrap-host.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+echo "-----------------------------------------------------------------------------------"
+echo "           CHI (Container Hosting Infrastructure) Bootstrap Host Script"
+echo "-----------------------------------------------------------------------------------"
+echo "This script will install the necessary software on the host machine to run the CHI"
+echo "environment. This script is intended to be run on a fresh Ubuntu 22.04 or 24.04 LTS install."
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root"
+  exit
+fi
+
+# Check if system is an ubuntu 22.04 LTS or higher
+if [ "$(lsb_release -rs)" != "22.04" ] && [ "$(lsb_release -rs)" != "24.04" ]; then
+  echo "This script is intended to be run on Ubuntu 22.04 or 24.04 LTS"
+  exit
+fi
+
+# Update and upgrade the system
+apt update && apt upgrade -y
+apt install acl git -y
+
+# Install Docker
+echo "-----------------------------------------------------------------------------------"
+echo "          CHI (Container Hosting Infrastructure) Removing Old Docker Packages"
+echo "-----------------------------------------------------------------------------------"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+
+# Install Docker
+echo "-----------------------------------------------------------------------------------"
+echo "          CHI (Container Hosting Infrastructure) Docker Installation"
+echo "-----------------------------------------------------------------------------------"
+# Add Docker's official GPG key:
+sudo apt-get -y update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# loop through array of directories and create directories
+echo "-----------------------------------------------------------------------------------"
+echo "          CHI (Container Hosting Infrastructure) Directory Creation"
+echo "-----------------------------------------------------------------------------------"
+CHI_DIRS=("/chi" "/chi/bin" "/chi/svc" "/chi/log" "/chi/tmp")
+for dir in "${CHI_DIRS[@]}"; do
+    echo "Creating directory: $dir"
+    mkdir -pv "$dir"
+done
