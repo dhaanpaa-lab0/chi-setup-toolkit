e9c4a047dbc9e1830016b70dbe2ca2edb53603b7
Daniel Haanpaa [Lab0] <dhaanpaa@djhent.us>
2024-07-23 23:19:55 -0700

    Added bootstrap fix

--- bootstrap-host.sh
+++ bootstrap-host.sh
@@ -25,7 +25,7 @@ apt install acl git -y
 echo "-----------------------------------------------------------------------------------"
 echo "          CHI (Container Hosting Infrastructure) Removing Old Docker Packages"
 echo "-----------------------------------------------------------------------------------"
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
 
 
 # Install Docker
@@ -33,19 +33,19 @@ echo "--------------------------------------------------------------------------
 echo "          CHI (Container Hosting Infrastructure) Docker Installation"
 echo "-----------------------------------------------------------------------------------"
 # Add Docker's official GPG key:
-sudo apt-get -y update
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get -y update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get -y update
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # loop through array of directories and create directories
 echo "-----------------------------------------------------------------------------------"
